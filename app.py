0f26e1d5d555ddbd17fe1fd9b4329a355b33da4d
Ben McNelly <ben@fbstudios.com>
2023-02-21 10:58:56 -0600

    why?

--- app.py
+++ app.py
@@ -9,7 +9,16 @@ def index():
         'index.html',
         title='Welcome to BenMcNelly.com',
         content="Welcome to BenMcNelly.com")
-        
+
+
+@app.route('/why')
+def why():
+    return render_template(
+        'why.html',
+        title='Why?',
+        content="If what you are doing is not important, then why are you doing it? Work on problems that might have"
+                " a future of becoming important to solve. ~Ben")
+
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
